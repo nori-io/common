d3ce1321e669e20bc3ff12552e5cf7c75c0e1ebe
Sergey <serge@cheebo.me>
2019-04-29 02:50:59 +0500

    removed fatal from logger/logger

--- logger/logger.go
+++ logger/logger.go
@@ -9,7 +9,6 @@ type Logger interface {
 	Warn(args ...interface{})
 	Warning(args ...interface{})
 	Error(args ...interface{})
-	Fatal(args ...interface{})
 
 	Debugf(format string, args ...interface{})
 	Infof(format string, args ...interface{})
@@ -17,7 +16,6 @@ type Logger interface {
 	Warnf(format string, args ...interface{})
 	Warningf(format string, args ...interface{})
 	Errorf(format string, args ...interface{})
-	Fatalf(format string, args ...interface{})
 
 	Debugln(args ...interface{})
 	Infoln(args ...interface{})
@@ -25,7 +23,6 @@ type Logger interface {
 	Warnln(args ...interface{})
 	Warningln(args ...interface{})
 	Errorln(args ...interface{})
-	Fatalln(args ...interface{})
 
 	WithField(key string, value interface{}) Logger
 	WithFields(fields Fields) Logger
