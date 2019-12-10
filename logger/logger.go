d2278077ca659e109e58f0139cf58e5d02f3e7a7
anita <bruteforce1414@gmail.com>
2019-12-10 16:16:10 +0500

    Sort functions in LoggerField interface

--- logger/logger.go
+++ logger/logger.go
@@ -11,24 +11,24 @@ type Field struct {
 }
 
 type FieldLogger interface {
-	// Panic logs a message with panic level and then throws the panic
-	Panic(format string, opts ...interface{})
-	// Fatal logs a message with fatal level and exit with status set to 1
-	Fatal(format string, opts ...interface{})
 	// Critical push to log entry with critical level
 	Critical(format string, opts ...interface{})
-	// Error push to log entry with error level
-	Error(format string, opts ...interface{})
-	// Warning push to log entry with warning level
-	Warning(format string, opts ...interface{})
-	// Notice push to log entry with notice level
-	Notice(format string, opts ...interface{})
-	// Info push to log entry with info level
-	Info(format string, opts ...interface{})
 	// Debug push to log entry with debug level
 	Debug(format string, opts ...interface{})
-	// Printf is like fmt.Printf, push to log entry with debug level
-	Printf(format string, opts ...interface{})
+	// Fatal logs a message with fatal level and exit with status set to 1
+	Fatal(format string, opts ...interface{})
+	// Error push to log entry with error level
+	Error(format string, opts ...interface{})
+	// Info push to log entry with info level
+	Info(format string, opts ...interface{})
 	// Log push to log with specified level
 	Log(level Level, format string, opts ...interface{})
+	// Notice push to log entry with notice level
+	Notice(format string, opts ...interface{})
+	// Panic logs a message with panic level and then throws the panic
+	Panic(format string, opts ...interface{})
+	// Printf is like fmt.Printf, push to log entry with debug level
+	Printf(format string, opts ...interface{})
+	// Warning push to log entry with warning level
+	Warning(format string, opts ...interface{})
 }
