04b49a67deb10d2724ba8ab6812089801b04d2c8
Sergey <serge@cheebo.me>
2020-01-10 20:11:18 +0500

    changed storage interface

--- storage/storage.go
+++ storage/storage.go
@@ -4,41 +4,59 @@ import "fmt"
 
 type (
 	Storage interface {
-		SQL() SQL
-		KV(schema string) KeyValue
+		// CreateBucket creates a new bucket with the given name and returns it.
+		CreateBucket(name string) (Bucket, error)
+		// CreateBucketIfNotExists creates a new bucket if it doesn't already exist and returns it.
+		CreateBucketIfNotExists(name string) (Bucket, error)
+		// Bucket returns a bucket by name.
+		Bucket(name string) (Bucket, error)
+		// DeleteBucket deletes a bucket with the given name.
+		DeleteBucket(name string) error
+	}
+
+	// Bucket represents a collection of key/value pairs
+	Bucket interface {
+		KeyValue
+		Cursor() Cursor
 	}
 
 	KeyValue interface {
+		// Get retrieves the value for a key.
 		Get(key string) ([]byte, error)
+		// Set sets the value for a key.
 		Set(key string, value []byte) error
+		// Delete removes a key
 		Delete(key string) error
+		// ForEach executes a function for each key/value pair
+		ForEach(fn func(k string, v []byte) error) error
 	}
 
-	SQL interface {
-		Create(item interface{}) error
-		Read(item interface{}) error
-		Update(item interface{}) error
-		Delete(item interface{}) error
-		Find(out interface{}, where ...interface{}) error
-		Migrate() Migrate
-	}
-
-	Migrate interface {
-		HasTable(name string) bool
-		CreateTable(models ...interface{}) error
-		DropTable(models ...interface{}) error
-		Migrate(models ...interface{})
-	}
-
-	Table interface {
-		TableName() string
+	Cursor interface {
+		First() (key string, value []byte)
+		Last() (key string, value []byte)
+		Next() (key string, value []byte)
+		Prev() (key string, value []byte)
+		// Seek moves the cursor to a seek key and returns it,
+		// If the key does not exist then then next key is used.
+		// If there are no keys, a nil key is returned
+		Seek(seek string) (key string, value []byte)
+		// Delete removes current key-value
+		Delete() error
 	}
 
 	NotFound struct {
+		Entity string
+	}
+
+	AlreadyExists struct {
 		Entity string
 	}
 )
 
 func (e NotFound) Error() string {
 	return fmt.Sprintf("Entity %s not found", e.Entity)
+}
+
+func (e AlreadyExists) Error() string {
+	return fmt.Sprintf("Entity %s already exists", e.Entity)
 }
